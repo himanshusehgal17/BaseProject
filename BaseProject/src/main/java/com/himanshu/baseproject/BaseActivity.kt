6abfa7043ff04dddddde8ff3a9088e7d3e4dbeb0
=himanshucubastion <=himanshu.sehgal@cubastion.com>
2022-06-13 18:00:25 +0530

    default to public

--- BaseProject/src/main/java/com/himanshu/baseproject/BaseActivity.kt
+++ BaseProject/src/main/java/com/himanshu/baseproject/BaseActivity.kt
@@ -3,9 +3,9 @@ package com.himanshu.baseproject
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class BaseActivity : AppCompatActivity(){
+public class BaseActivity : AppCompatActivity(){
 
-    fun showToast(msg:String) {
+   public fun showToast(msg:String) {
         Toast.makeText(this,msg,Toast.LENGTH_LONG).show()
     }
 }
